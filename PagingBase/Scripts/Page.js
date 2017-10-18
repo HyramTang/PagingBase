c53f1c4712fb08ceffd6572d1660a7d7d6682557
Hyram <hyram.tang@gmail.com>
2017-10-18 21:59:44 +0800

    Fixed bugs!

--- PagingBase/Scripts/Page.js
+++ PagingBase/Scripts/Page.js
@@ -1,22 +1,22 @@
 ﻿var Paging = (function () {
-    var pageContent = function (page, pagetotal) {
+    var pageContent = function (pageindex, pagetotal) {
         //生成分页HTML样式代码
         var str = "";
-        var pre = '<li><a href="javascript:changePage(' + (page - 1) + ')" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>', next = '<li><a href="javascript:changePage(' + (page + 1) + ')" aria-label="Previous"><span aria-hidden="true">&raquo;</span></a></li>';
+        var pre = '<li><a href="javascript:changePage(' + (pageindex - 1) + ')" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>', next = '<li><a href="javascript:changePage(' + (pageindex + 1) + ')" aria-label="Previous"><span aria-hidden="true">&raquo;</span></a></li>';
 
-        if (page <= 3) {
+        if (pageindex <= 3) {
             for (var i = 1; i <= 5; i++) {
-                str += '<li class=\"' + (i == page ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
+                str += '<li class=\"' + (i == pageindex ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
             }
             str = str + next;
-        } else if (page >= (pagetotal - 4)) {
+        } else if (pageindex >= (pagetotal - 4)) {
             for (var i = (pagetotal - 4) ; i <= pagetotal; i++) {
-                str += '<li class=\"' + (i == page ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
+                str += '<li class=\"' + (i == pageindex ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
             }
             str = pre + str;
         } else {
-            for (var i = (page - 2) ; i <= page + 2; i++) {
-                str += '<li class=\"' + (i == page ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
+            for (var i = (pageindex - 2) ; i <= pageindex + 2; i++) {
+                str += '<li class=\"' + (i == pageindex ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
             }
             str = pre + str + next;
         }

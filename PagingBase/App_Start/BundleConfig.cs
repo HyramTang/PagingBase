fb4f1ba0a45f7262cebd953c82263adbca171aa0
HyramTang <hyram.tang@gmail.com>
2017-12-09 22:39:39 +0800

    Update page.js & fixed bugs

--- PagingBase/App_Start/BundleConfig.cs
+++ PagingBase/App_Start/BundleConfig.cs
@@ -11,8 +11,10 @@ namespace PagingBase.App_Start
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/Page.js"));
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customJS").Include(
+                "~/Scripts/Page.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css"));
--- PagingBase/Scripts/Page.js
+++ PagingBase/Scripts/Page.js
@@ -1,30 +1,27 @@
 ﻿var Paging = (function () {
     var pageContent = function (pageindex, pagetotal) {
         //生成分页HTML样式代码
-        var str = "";
-        var pre = '<li><a href="javascript:changePage(' + (pageindex - 1) + ')" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>', next = '<li><a href="javascript:changePage(' + (pageindex + 1) + ')" aria-label="Previous"><span aria-hidden="true">&raquo;</span></a></li>';
+        var str = "",
+            prePageIndex = (pageindex - 1) <= 0 ? pagetotal : (pageindex - 1),
+            nexPageIndex = (pageindex + 1) > pagetotal ? 1 : (pageindex + 1);
+        var pre = '<li><a href="javascript:changePage(' + prePageIndex + ')" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>',
+            next = '<li><a href="javascript:changePage(' + nexPageIndex + ')" aria-label="Previous"><span aria-hidden="true">&raquo;</span></a></li>';
 
         if (pageindex <= 3) {
             for (var i = 1; i <= 5; i++) {
                 str += '<li class=\"' + (i == pageindex ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
             }
-            str = str + next;
-        } else if (pageindex >= (pagetotal - 4)) {
+        } else if (pageindex > (pagetotal - 3)) {
             for (var i = (pagetotal - 4) ; i <= pagetotal; i++) {
                 str += '<li class=\"' + (i == pageindex ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
             }
-            str = pre + str;
         } else {
             for (var i = (pageindex - 2) ; i <= pageindex + 2; i++) {
                 str += '<li class=\"' + (i == pageindex ? 'active' : '') + '\"><a href="javascript:changePage(' + i + ')">' + i + '</a></li>';
             }
-            str = pre + str + next;
         }
-        
-        var ssss = '<ul class="pagination">' + str + '</ul>';
-        console.info(ssss);
-
-        return ssss;
+        str = pre + str + next;
+        return '<ul class="pagination">' + str + '</ul>';
     }
 
     return pageContent;

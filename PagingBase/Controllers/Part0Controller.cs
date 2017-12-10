397f659341a2f6d7d924ef01bbd3ba8e4c86f9d2
HyramTang <hyram.tang@gmail.com>
2017-12-10 17:23:09 +0800

    Add Part0：Pagination generate content！

---
+++ PagingBase/Controllers/Part0Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PagingBase.Controllers
+{
+    public class Part0Controller : Controller
+    {
+        // GET: Part0
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

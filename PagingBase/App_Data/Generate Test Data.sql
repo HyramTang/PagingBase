3987032e3496a9c31e7d133cd677fca4a994d0c6
HyramTang <hyram.tang@gmail.com>
2017-12-17 22:22:01 +0800

    Add Generate Test Data Script

---
+++ PagingBase/App_Data/Generate Test Data.sql
@@ -0,0 +1,12 @@
+﻿--循环插入数据
+declare @i int=0
+while @i<320
+begin
+set @i=@i+1;
+declare @str nvarchar(50)=convert(nvarchar(50),@i);
+insert into dbo.PagingData values(@i,'A'+@str,'B'+@str,'C'+@str)
+end
+
+select * from PagingData
+
+truncate table pagingdata
